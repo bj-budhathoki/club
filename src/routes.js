ae1a40dd2ad62da46c112aeffcffde1b49c61481
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-12-02 13:49:49 +0545

    diplay mateches

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,26 @@ import Signin from "./Components/signin";
 import Layout from "./HOC/Layout";
 import Home from "./Components/home/";
 import Dashboard from "./Components/admin/Dashboard";
+import PrivateRoutes from "./Components/authRoutes/privateRoutes";
+import AdminMatches from "./Components/admin/matches";
 const Routes = props => {
   return (
     <Layout>
       <Switch>
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/sign_in" component={Signin} />
+        <PrivateRoutes
+          {...props}
+          path="/admin_matches"
+          exact
+          component={AdminMatches}
+        />
         <Route exact path="/" component={Home} />
+        <Route exact path="/sign_in" component={Signin} />
+        <PrivateRoutes
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
       </Switch>
     </Layout>
   );
